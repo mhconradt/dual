01c4ffbbc2fc1c4449ef543325331b6d0ab5bc2a
mhconradt <mhconradt@protonmail.com>
2024-05-28 14:49:42 -0700

    Support arbitrary models

--- Dual.py
+++ Dual.py
@@ -15,10 +15,7 @@ st.set_page_config(layout="wide")
 st.title("Dual")
 
 with st.sidebar:
-    oai_model = st.selectbox("OpenAI Model", ["gpt-4o", "gpt-4-turbo"])
-    ant_model = st.selectbox("Anthropic Model",
-                             ["claude-3-opus-20240229", "claude-3-sonnet-20240229", "claude-3-haiku-20240229"])
-
+    models = st.multiselect("Models", ["claude-3-opus-20240229", "claude-3-sonnet-20240229", "claude-3-haiku-20240229", "gpt-4o", "gpt-4-turbo"], default=["claude-3-opus-20240229", "gpt-4o"])
 
 def custom_logger(log):
     caller_frame = inspect.currentframe().f_back
@@ -37,16 +34,23 @@ class Message:
 
 
 class ChatCompletionEngine(ABC):
+    def __init__(self, model: str):
+        self.model = model
+
+    @classmethod
+    def for_model(cls, model: str) -> "ChatCompletionEngine":
+        return OpenAIEngine(model) if model.startswith("gpt") else AnthropicEngine(model)
+        
     @abstractmethod
     async def complete(self, messages: list[Message], model: str) -> Message:
         pass
 
 
 class OpenAIEngine(ChatCompletionEngine):
-    async def complete(self, messages: list[Message], model: str) -> Message:
+    async def complete(self, messages: list[Message]) -> Message:
         c = AsyncOpenAI()
         resp = await c.chat.completions.create(
-            model=model,
+            model=self.model,
             max_tokens=1024,
             messages=[dataclasses.asdict(m) for m in messages]
         )
@@ -57,14 +61,14 @@ class OpenAIEngine(ChatCompletionEngine):
 
 class AnthropicEngine(ChatCompletionEngine):
 
-    async def complete(self, messages: list[Message], model: str) -> Message:
+    async def complete(self, messages: list[Message]) -> Message:
         c = anthropic.AsyncAnthropic()
         messages_ = [dataclasses.asdict(m) for m in messages if m.role != "system"]
         system = next(iter(m.content for m in messages if m.role == "system"), None)
         completion = await c.messages.create(
             max_tokens=1024,
             messages=messages_,
-            model=model,
+            model=self.model,
             system=system
         )
         content = completion.content[0].text
@@ -80,17 +84,8 @@ if 'messages' not in st.session_state:
         )
     ]
 
-if 'openai' not in st.session_state:
-    st.session_state.openai = OpenAIEngine()
-if 'anthropic' not in st.session_state:
-    # golden gate bridge claude coming soon
-    st.session_state.anthropic = AnthropicEngine()
-
-if 'oai_cmp' not in st.session_state:
-    st.session_state.oai_cmp = None
-
-if 'ant_cmp' not in st.session_state:
-    st.session_state.ant_cmp = None
+if 'completions' not in st.session_state:
+    st.session_state.completions = ()
 
 
 def printm(m: Message, out: DeltaGenerator | None = None):
@@ -117,7 +112,7 @@ def chat_input() -> Message | None:
 
 def get_user_selection():
     with st.container():
-        selection = st.radio("Choose the best completion", ["OpenAI", "Anthropic"], index=None, horizontal=True)
+        selection = st.radio("Choose the best completion", models, index=None, horizontal=True)
         custom_logger(f"Got user selection {selection} from radio component.")
         return selection
 
@@ -131,22 +126,17 @@ def run_gather(*coros):
 
 if prompt_msg := chat_input():
     st.session_state.messages.append(prompt_msg)
-    printm(prompt_msg, None)
-    st.session_state.oai_cmp, st.session_state.ant_cmp = run_gather(
-        st.session_state.openai.complete(st.session_state.messages, model=oai_model),
-        st.session_state.anthropic.complete(st.session_state.messages, model=ant_model),
-    )
-if any(x is not None for x in (st.session_state.oai_cmp, st.session_state.ant_cmp)):
-    oai, ant = st.columns(2)
-    with oai:
-        st.markdown(f"<h3 style='text-align: center;'>OpenAI ({oai_model})</h3>", unsafe_allow_html=True)
-        printm(st.session_state.oai_cmp)
-    with ant:
-        st.markdown(f"<h3 style='text-align: center;'>Anthropic ({ant_model})</h3>", unsafe_allow_html=True)
-        printm(st.session_state.ant_cmp)
+    printm(prompt_msg)
+    st.session_state.completions = run_gather(*(ChatCompletionEngine.for_model(model).complete(st.session_state.messages) for model in models))
+if any(x is not None for x in st.session_state.completions):
+    columns = st.columns(len(models))
+    for model_, completion_, col_ in zip(models, st.session_state.completions, columns):
+        with col_:
+            st.markdown(f"<h3 style='text-align: center;'>{model_}</h3>", unsafe_allow_html=True)
+            printm(completion_)
     if user_selection := get_user_selection():
         st.session_state.messages.append(
-            st.session_state.oai_cmp if user_selection == "OpenAI" else st.session_state.ant_cmp
+            next(cmp for cmp, model in zip(st.session_state.completions, models) if model == user_selection)
         )
-        st.session_state.oai_cmp, st.session_state.ant_cmp = None, None
+        st.session_state.completions = ()
         st.rerun()
