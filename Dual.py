c55f081c664f56988742066be2b261fcc2f0e34f
Max Conradt <43765720+mhconradt@users.noreply.github.com>
2024-07-11 22:00:37 -0700

    Add Claude 3.5 Sonnet (#3)

--- Dual.py
+++ Dual.py
@@ -23,7 +23,7 @@ TOOL_ALIASES = {'run_shell_script': 'Shell'}
 st.set_page_config(layout="wide")
 st.title("Dual")
 
-ANTHROPIC_MODELS = ["claude-3-opus-20240229", "claude-3-sonnet-20240229", "claude-3-haiku-20240307"]
+ANTHROPIC_MODELS = ["claude-3-5-sonnet-20240620", "claude-3-opus-20240229", "claude-3-sonnet-20240229", "claude-3-haiku-20240307"]
 OPENAI_MODELS = ["gpt-4-0125-preview", "gpt-4o", "gpt-4-turbo", "gpt-4-turbo-2024-04-09", "gpt-4-1106-preview",
                  "gpt-4-32k-0613", "gpt-3.5-turbo-0125", "gpt-3.5-turbo-1106"]
 
